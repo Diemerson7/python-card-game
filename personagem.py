c4021c1d34e9f597f5afe081ac4b5be96f25b942
diemerson <diemersondematoss@gmail.com>
2025-06-11 17:04:36 -0300

    11.06

---
+++ personagem.py
@@ -0,0 +1,27 @@
+class Personagem:
+    def __init__(self, nome):
+        self.nome =  nome
+        self.vida_maxima = 100
+        self.vida = 100
+        self.ataque = 70
+        self.defesa = 50
+        self.cartas = []
+        self.energia = 10
+        
+    def atacar():
+        pass
+    
+    def usar_carta():
+        pass
+    
+    def comprar_carta():
+        pass
+    
+    def levar_dano():
+        pass
+    
+    def ver_lista_cartas():
+        pass
+    
+    def curar_se():
+        pass
