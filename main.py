f07ac6656561189297d8703c74cf37555b53dc03
diemerson <diemersondematoss@gmail.com>
2025-06-13 17:14:42 -0300

    13.06

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from personagem import Personagem 
+from personagem import Personagem, CartaAumento
 
-objeto_personagem1 = Personagem("Diemerson")
-mensagem = objeto_personagem1.falar_nome()
-print(mensagem)
+heroi = Personagem("Herói")
+carta1 = CartaAumento("Herói", 20, "Carta que aumenta o Ataque", 3, 5)
+print(f"Ataque do Herói: {heroi.ataque}.")
+carta1.usar_carta(personagem = heroi)
+print(f"Ataque do Herói: {heroi.ataque}.")
+
+carta2 = CartaAumento("Herói", 30, "Carta que aumenta a Defesa", )
--- personagem.py
+++ personagem.py
@@ -1,5 +1,6 @@
-class Personagem:
+class Personagem:  
     def __init__(self, nome):
+        # Atributos
         self.nome =  nome
         self.vida_maxima = 100
         self.vida = 100
@@ -7,21 +8,46 @@ class Personagem:
         self.defesa = 50
         self.cartas = []
         self.energia = 10
+
+class Carta:
+    def __init__(self, nome, energia_gasta, descricao):
+        # Atributos
+        self.nome = nome
+        self.energia_gasta = energia_gasta
+        self.descricao = descricao
+
+    # Métodos
+    def usar_carta(self):
+        pass
+    
+class CartaAumento(Carta):
+    # O "init" é o construtor
+    def __init__(self, nome, energia_gasta, descricao, tipo, pontos_aumentados):
+        super().__init__(nome, energia_gasta, descricao)
+        self.tipo = tipo
+        self.pontos_aumentados = pontos_aumentados
+    
+    def usar_carta(self, personagem):
+        if self.tipo == 1: 
+            personagem.vida_maxima = self.pontos_aumentados + personagem.vida_maxima
+            print(f"{self.nome} usou a carta de Aumento de Vida Máxima.")
+            return
         
-    def atacar():
-        pass
-    
-    def usar_carta():
-        pass
-    
-    def comprar_carta():
-        pass
-    
-    def levar_dano():
-        pass
-    
-    def ver_lista_cartas():
-        pass
-    
-    def curar_se():
-        pass
+        if self.tipo == 2:
+            personagem.defesa = self.pontos_aumentados + personagem.defesa
+            print(f"{self.nome} usou a carta de Aumento de Defesa.")
+            return
+        
+        if self.tipo == 3:
+            personagem.ataque = self.pontos_aumentados + personagem.ataque
+            print(f"{self.nome} Usou a carta de Aumento de Ataque.")
+            return
+        
+        if self.tipo == 4:
+            personagem.energia_maxima = self.pontos_aumentados + personagem.energia_maxima
+            print(f"{self.nome} Usou a carta de Aumento de Energia Máxima.")
+            return
+        
+class CartaRoubo(Carta):
+    def __init__(self, nome, energia_gasta, descricao):
+        super().__init__(nome, energia_gasta, descricao)
