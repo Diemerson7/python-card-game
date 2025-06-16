9c7d4415d266622246e1e40843c952dfa60d9216
diemerson <diemersondematoss@gmail.com>
2025-06-16 17:03:38 -0300

    16.06

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from personagem import Personagem, CartaAumento
+from personagem import Personagem, CartaAumento, CartaRoubo, CartaAtordoamento, CartaDano, CartaCura
 
 heroi = Personagem("Herói")
-carta1 = CartaAumento("Herói", 20, "Carta que aumenta o Ataque", 3, 5)
+carta1 = CartaAumento("Ataque", 20, "Carta que aumenta o Ataque", 3, 5) # O número "3" é o número do tipo da carta. Por exemplo, o número do ataque é 3, porque é o "self.tipo == 3", e o número "5" é a quantidade que vai aumentar do ataque.
 print(f"Ataque do Herói: {heroi.ataque}.")
 carta1.usar_carta(personagem = heroi)
 print(f"Ataque do Herói: {heroi.ataque}.")
 
-carta2 = CartaAumento("Herói", 30, "Carta que aumenta a Defesa", )
+carta2 = CartaAumento("Defesa", 20, "Carta que aumenta a Defesa", 2, 5)
+print(f"Defesa do Herói: {heroi.defesa}.")
+carta2.usar_carta(personagem = heroi)
+print(f"Defesa do Herói: {heroi.defesa}.")
+
+carta3 = CartaAumento("Vida Máxima", 30, "Carta que aumenta a Vida Máxima", 1, 10)
+print(f"Vida Máxima do Herói: {heroi.vida_atual}.")
+carta3.usar_carta(personagem = heroi)
+print(f"Vida Máxima do Herói: {heroi.vida_atual}.")
+
+carta4 = CartaAumento("Energia Máxima", 30, "Carta que aumenta a Energia Máxima", 4, 5)
+print(f"Energia Máxima: {heroi.energia}")
+carta4.usar_carta(personagem = heroi)
+print(f"Energia Máxima: {heroi.energia}")
+
+carta5 = CartaRoubo("Carta Roubo", 30, "Carta que rouba uma carta aleatória do adversário")
+
+carta6 = CartaAtordoamento("Carta de Atordoamento", 10, "Carta que zera a energia do adversário", 0)
+
+carta7 = CartaDano("Carta de Dano", 20, "Carta que dá dano no adversário", 10)
+
+carta8 = CartaCura("Carta de Cura", 20, "Carta que cura a vida", 10)
--- personagem.py
+++ personagem.py
@@ -1,13 +1,16 @@
+import random
+
 class Personagem:  
     def __init__(self, nome):
         # Atributos
         self.nome =  nome
         self.vida_maxima = 100
-        self.vida = 100
-        self.ataque = 70
-        self.defesa = 50
+        self.vida_atual = 100
+        self.ataque = 30
+        self.defesa = 40
         self.cartas = []
-        self.energia = 10
+        self.energia = 100
+        self.energia_maxima = 100
 
 class Carta:
     def __init__(self, nome, energia_gasta, descricao):
@@ -24,18 +27,19 @@ class CartaAumento(Carta):
     # O "init" é o construtor
     def __init__(self, nome, energia_gasta, descricao, tipo, pontos_aumentados):
         super().__init__(nome, energia_gasta, descricao)
+        # Atributos
         self.tipo = tipo
         self.pontos_aumentados = pontos_aumentados
     
     def usar_carta(self, personagem):
         if self.tipo == 1: 
             personagem.vida_maxima = self.pontos_aumentados + personagem.vida_maxima
-            print(f"{self.nome} usou a carta de Aumento de Vida Máxima.")
+            print(f"{self.nome} Usou a carta de Aumento de Vida Máxima.")
             return
         
         if self.tipo == 2:
             personagem.defesa = self.pontos_aumentados + personagem.defesa
-            print(f"{self.nome} usou a carta de Aumento de Defesa.")
+            print(f"{self.nome} Usou a carta de Aumento de Defesa.")
             return
         
         if self.tipo == 3:
@@ -51,3 +55,37 @@ class CartaAumento(Carta):
 class CartaRoubo(Carta):
     def __init__(self, nome, energia_gasta, descricao):
         super().__init__(nome, energia_gasta, descricao)
+        
+    def usar_carta(self, vitima: Personagem, ladrao: Personagem):
+        sorteio_carta_roubada = random.randint(0, len(vitima.cartas)-1) 
+        carta_roubada = vitima.cartas.pop(sorteio_carta_roubada) # Rouba a carta da Vítima
+        ladrao.cartas.append(carta_roubada) # Carta da Vítima vem para mim
+        return  
+        
+    
+class CartaAtordoamento(Carta):
+    def __init__(self, nome, energia_gasta, descricao):
+        super().__init__(nome, energia_gasta, descricao)
+        
+    def usar_carta(self, vitima: Personagem):
+        vitima.energia = 0
+        return
+        
+    
+class CartaDano(Carta):
+    def __init__(self, nome, energia_gasta, descricao, pontos_de_dano):
+        super().__init__(nome, energia_gasta, descricao)
+        self.pontos_de_dano = pontos_de_dano
+
+    def usar_carta(self, inimigo: Personagem, causador: Personagem):
+        inimigo.vida_atual -= causador.ataque - inimigo.defesa
+        
+        
+class CartaCura(Carta):
+    def __init__(self, nome, energia_gasta, descricao, pontos_de_vida_curado):
+        super().__init__(nome, energia_gasta, descricao)
+        self.pontos_de_vida_curado = pontos_de_vida_curado
+        
+    def usar_carta(self, beneficiario: Personagem):
+        beneficiario.vida_atual += beneficiario.vida_maxima * 0.30  # Cura 30% da vida máxima
+           
